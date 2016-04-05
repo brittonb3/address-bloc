9d5e4b5a60eb468d1cbca0974ff83d298bbb367d
Britton Baumann <brittonbaumann@gmail.com>
2016-04-05 00:13:35 -0500

    Assignment Address Bloc Models

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,7 +5,7 @@ class AddressBook
 
   def initialize
     @entries = []
-    end
+  end
 
   def add_entry(name, phone_number, email)
     index = 0
@@ -16,5 +16,16 @@ class AddressBook
       index += 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+      entries.delete(delete_entry)
+    end
   end
 end
