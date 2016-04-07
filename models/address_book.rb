f4938f7f35c9c270314a63a982c71521522b131f
Britton Baumann <brittonbaumann@gmail.com>
2016-04-07 09:42:39 -0500

    Assignment Address Bloc Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,6 +44,16 @@ class AddressBook
         lower = mid + 1
       end
     end
-    return nil      
+    return nil
   end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+  end
+
 end
